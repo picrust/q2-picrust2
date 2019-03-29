03875784ba31d70b04f5c4d38dfa4ea3e4ccf1d7
Gavin Douglas <gavinmdouglas@gmail.com>
2019-03-29 19:57:24 +0000

    updated to be compatible with qiime2 2019-0.1

--- q2_picrust2/_custom_tree_pipeline.py
+++ q2_picrust2/_custom_tree_pipeline.py
@@ -13,7 +13,6 @@ def custom_tree_pipeline(table: biom.Table,
                          hsp_method: str = "mp",
                          max_nsti: float = 2.0)  -> (biom.Table,
                                                      biom.Table,
-                                                     biom.Table,
                                                      biom.Table):
 
     # Run pipeline in temporary directory so that files are not saved locally.
@@ -34,31 +33,41 @@ def custom_tree_pipeline(table: biom.Table,
 
         # Run hidden-state prediction step (on 16S, EC, and KO tables
         # separately.
-        hsp_out_16S = path.join(picrust2_out, "16S_predicted")
-        system_call_check("hsp.py -i 16S -t " + newick_infile + " -p 1 -n "
-                          "-o " + hsp_out_16S + " -m " + hsp_method)
+        hsp_out_16S = path.join(picrust2_out, "16S_predicted.tsv")
+        system_call_check("hsp.py -i 16S " +
+                          " -t " + newick_infile +
+                          " -p 1 " +
+                          " -n " +
+                          "-o " + hsp_out_16S +
+                          " -m " + hsp_method)
 
-        hsp_out_EC = path.join(picrust2_out, "EC_predicted")
-        system_call_check("hsp.py -i EC -t " + newick_infile + " -p " +
-                          str(threads) + " -o " + hsp_out_EC + " -m " +
-                          hsp_method)
+        hsp_out_EC = path.join(picrust2_out, "EC_predicted.tsv")
+        system_call_check("hsp.py -i EC " +
+                          " -t " + newick_infile +
+                          " -p " + str(threads) +
+                          " -o " + hsp_out_EC +
+                          " -m " + hsp_method)
 
-        hsp_out_KO = path.join(picrust2_out, "KO_predicted")
-        system_call_check("hsp.py -i KO -t " + newick_infile + " -p " +
-                          str(threads) + " -o " + hsp_out_KO + " -m " +
-                          hsp_method)
+        hsp_out_KO = path.join(picrust2_out, "KO_predicted.tsv")
+        system_call_check("hsp.py -i KO " + 
+                          " -t " + newick_infile +
+                          " -p " + str(threads) +
+                          " -o " + hsp_out_KO +
+                          " -m " + hsp_method)
 
         # Run metagenome pipeline step.
         EC_metagenome_out = path.join(picrust2_out, "EC_metagenome_out")
-        system_call_check("metagenome_pipeline.py -i " + biom_infile + " -m " +
-                          hsp_out_16S + ".tsv -f " + hsp_out_EC + ".tsv -p " +
-                          str(threads) + " -o " + EC_metagenome_out +
+        system_call_check("metagenome_pipeline.py -i " + biom_infile +
+                          " -m " + hsp_out_16S +
+                          " -f " + hsp_out_EC +
+                          " -o " + EC_metagenome_out +
                           " --max_nsti " + str(max_nsti))
 
         KO_metagenome_out = path.join(picrust2_out, "KO_metagenome_out")
-        system_call_check("metagenome_pipeline.py -i " + biom_infile + " -m " +
-                          hsp_out_16S + ".tsv -f " + hsp_out_KO + ".tsv -p " +
-                          str(threads) + " -o " + KO_metagenome_out +
+        system_call_check("metagenome_pipeline.py -i " + biom_infile +
+                          " -m " + hsp_out_16S +
+                          " -f " + hsp_out_KO +
+                          " -o " + KO_metagenome_out +
                           " --max_nsti " + str(max_nsti))
 
         # Run pathway inference step.
@@ -66,18 +75,17 @@ def custom_tree_pipeline(table: biom.Table,
 
         EC_out = path.join(EC_metagenome_out, "pred_metagenome_unstrat.tsv")
 
-        system_call_check("run_minpath.py -i " + EC_out + " -o " +
-                          pathways_out + " -p " + str(threads))
+        system_call_check("pathway_pipeline.py -i " + EC_out +
+                          " -o " + pathways_out +
+                          " -p " + str(threads))
 
         # Read in output unstratified metagenome tables and return as BIOM
         # objects.
         KO_out = path.join(KO_metagenome_out, "pred_metagenome_unstrat.tsv")
         pathabun_out = path.join(pathways_out, "path_abun_unstrat.tsv")
-        pathcov_out = path.join(pathways_out, "path_cov_unstrat.tsv")
 
         ko_biom = biom.load_table(KO_out)
         ec_biom = biom.load_table(EC_out)
         pathabun_biom = biom.load_table(pathabun_out)
-        pathcov_biom = biom.load_table(pathcov_out)
 
-        return ko_biom, ec_biom, pathabun_biom, pathcov_biom
+        return ko_biom, ec_biom, pathabun_biom
